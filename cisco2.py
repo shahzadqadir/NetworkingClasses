2ee3c009b8ff53d867504d6884af0adb5c108d57
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2022-02-11 14:56:15 +0000

    update cisco2.py file

--- cisco2.py
+++ cisco2.py
@@ -10,34 +10,28 @@ class CiscoRouter:
         self.hostname = hostname
         self.username = username
         self.password = password
+        self.conn = paramiko.SSHClient()
+        self.conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.conn.connect(hostname=self.hostname, username=self.username, password=self.password, allow_agent=False, look_for_keys=False)
+        self.rtcon = self.conn.invoke_shell()
+        
+    def close_connection(self):
+        if self.conn != None:
+            self.conn.close()
+    
         
     def show_cmd_ssh(self, command):
         '''Takes a single command as a string and return output in a list format'''
-        conn = paramiko.SSHClient()
-        conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        conn.connect(hostname=self.hostname, username=self.username, password=self.password, allow_agent=False, look_for_keys=False)
-        stdin, stdout, stderr = conn.exec_command(command)
+        stdin, stdout, stderr = self.conn.exec_command(command)
         stdout = stdout.readlines()
         return stdout
     
     def config_cmd_ssh(self, cmd_list):
-        try:    
-            '''Takes a list of commands and push them to device using SSH. Returns true configuration was successful.
-            No capability to return error if connection was successful but command was wrong.'''
-            conn = paramiko.SSHClient()
-            conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            conn.connect(hostname=self.hostname, username=self.username, password=self.password, allow_agent=False, look_for_keys=False)
-        except paramiko.AuthenticationException:
-            return False
-        except paramiko.SSHException:
-            return False
-        
-        rtcon = conn.invoke_shell()
-        time.sleep(1)
-        rtcon.send('configure terminal\n')
+        '''Takes a list of commands and push them to device, this method don't close connection by itself.'''
+        self.rtcon.send('configure terminal\n')
         time.sleep(1)
         for command in cmd_list:
-            rtcon.send(command + '\n')
+            self.rtcon.send(command + '\n')
             time.sleep(1)
             
         return True
